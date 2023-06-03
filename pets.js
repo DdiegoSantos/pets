7ccfbab4d4917f613e6e5c92a8ee2b0f766f1aaf
Diego Nunes Santos <130621994+DdiegoSantos@users.noreply.github.com>
2023-06-02 23:09:49 -0300

    primeiro commit do site

---
+++ pets.js
@@ -0,0 +1,10 @@
+
+function trocarRight(){
+    let banner = document.querySelector('.box');
+    //let right = document.querySelector('.area-right');
+    //let left = document.querySelector('.area-left');
+    banner.style.max = 'translateX(-33.33%)'
+
+}//.box:hover{
+    //transform:translateX(-33.33%) ;
+   // transition: 2s;
